b70f564aeb755222a2ac12cbb561f1f996edba72
tvalimak <tvalimak@student.hive.fi>
2025-03-26 11:59:20 +0200

    added folder check and some cleaning of exessive stuff

--- ex00/BitcoinExchange.cpp
+++ ex00/BitcoinExchange.cpp
@@ -1,53 +1,3 @@
-/*You have to create a program which outputs the value of a certain amount of bitcoin
-on a certain date.
-This program must use a database in csv format which will represent bitcoin price
-over time. This database is provided with this subject.
-The program will take as input a second database, storing the different prices/dates
-to evaluate.
-Your program must respect these rules:
-• The program name is btc.
-• Your program must take a file as an argument.
-• Each line in this file must use the following format: "date | value".
-• A valid date will always be in the following format: Year-Month-Day.
-• A valid value must be either a float or a positive integer, between 0 and 1000.
-Here is an example of an input.txt file:
-$> head input.txt
-date | value
-2011-01-03 | 3
-2011-01-03 | 2
-2011-01-03 | 1
-2011-01-03 | 1.2
-2011-01-09 | 1
-2012-01-11 | -1
-2001-42-42
-2012-01-11 | 1
-2012-01-11 | 2147483648
-$>
-Your program will use the value in your input file.
-Your program should display on the standard output the result of the value multiplied
-by the exchange rate according to the date indicated in your database.
-If the date used in the input does not exist in your DB then you
-must use the closest date contained in your DB. Be careful to use the
-lower date and not the upper one.
-The following is an example of the program’s use.
-$> ./btc
-Error: could not open file.
-$> ./btc input.txt
-2011-01-03 => 3 = 0.9
-2011-01-03 => 2 = 0.6
-2011-01-03 => 1 = 0.3
-2011-01-03 => 1.2 = 0.36
-2011-01-09 => 1 = 0.32
-Error: not a positive number.
-Error: bad input => 2001-42-42
-2012-01-11 => 1 = 7.1
-Error: too large a number.
-$>
-Warning: The container(s) you use to validate this exercise will no
-longer be usable for the rest of this module.
-
-*/
-
 #include "BitcoinExchange.hpp"
 
 BitcoinExchange::BitcoinExchange()
@@ -77,7 +27,7 @@ void BitcoinExchange::readDatabase(const std::string &filename)
 	}
 	std::string
 		line;
-	std::getline(file, line);	// skip header	
+	std::getline(file, line);	
 	while (std::getline(file, line))
 	{
 		std::istringstream
@@ -178,38 +128,52 @@ std::string BitcoinExchange::findClosestDate(const std::string& date)
 
 void BitcoinExchange::readAndProcessInput(const std::string& filename)
 {
+	struct stat fileStat; 
+    if (stat(filename.c_str(), &fileStat) == 0) 
+    {
+        if (S_ISDIR(fileStat.st_mode)) 
+        {
+            std::cerr << "Error: the specified input is a directory, not a file." << std::endl;
+            return;
+        }
+    } else 
+    {
+        std::cerr << "Error: could not stat the file." << std::endl;
+        return;
+    }
 	std::ifstream file(filename);
 	if (!file.is_open())
 		throw std::runtime_error("Error: could not open input file.");
 	std::string line;
 	std::getline(file, line);
-	while (std::getline(file, line)) {
+	while (std::getline(file, line))
+	{
 		std::istringstream iss(line);
 		std::string dateStr;
 		double value;
-		if (!std::getline(iss, dateStr, '|')) {
-			//input.push_back(std::make_pair(line, "Error: bad input => " + line));
+		if (!std::getline(iss, dateStr, '|'))
+		{
 			std::cerr << "Error: bad input => " << line << std::endl;
 			continue;
 		}
 		dateStr = trim(dateStr);
-		if (!(iss >> value)) {
-			//input.push_back(std::make_pair(dateStr, "Error: bad input => " + line));
+		if (!(iss >> value))
+		{
 			std::cerr << "Error: bad input => " << line << std::endl;
 			continue;
 		}
-		if (!isValidDateFormat(dateStr) || !isValidDateValue(dateStr)) {
-			//input.push_back(std::make_pair(dateStr, "Error: bad input => " + dateStr));
+		if (!isValidDateFormat(dateStr) || !isValidDateValue(dateStr))
+		{
 			std::cerr << "Error: bad input => " << dateStr << std::endl;
 			continue;
 		}
-		if (value < 0) {
-			//input.push_back(std::make_pair(dateStr, "Error: not a positive number."));
+		if (value < 0)
+		{
 			std::cerr << "Error: not a positive number." << std::endl;
 			continue;
 		}
-		if (value > 1000) {
-			//input.push_back(std::make_pair(dateStr, "Error: too large a number."));
+		if (value > 1000)
+		{
 			std::cerr << "Error: too large a number." << std::endl;
 			continue;
 		}
@@ -228,6 +192,4 @@ void BitcoinExchange::run(const std::string &filename)
 {
 	readDatabase("data.csv");
 	readAndProcessInput(filename);
-	display();
 }
-
